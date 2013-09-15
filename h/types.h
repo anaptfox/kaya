ef822e3c6a4b57317895506aea45d0376c60de29
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-14 21:01:37 -0400

    Fixed commas in types.h

--- h/types.h
+++ h/types.h
@@ -24,11 +24,11 @@ typedef struct {
 /* process control block type */
 typedef struct pcb_t {
 	/* process queue fields */
-	struct pcb_t *p_next, /* pointer to next entry */
-	struct pcb_t *p_prev, /* pointer to previous  entry */
+	struct pcb_t *p_next; /* pointer to next entry */
+	struct pcb_t *p_prev; /* pointer to previous  entry */
 	/* process tree fields */
-		*p_prnt, /* pointer to parent */
-		*p_child, /* pointer to 1st child */
+		*p_prnt; /* pointer to parent */
+		*p_child; /* pointer to 1st child */
 		*p_sib; /* pointer to sibling */
 		*p_prev_sib; /* pointer to previous sibling */
 	
