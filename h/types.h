b3eddcb37f5ff0ded75be691a555065365cf7887
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-14 21:02:56 -0400

    syntax error in types.h

--- h/types.h
+++ h/types.h
@@ -24,12 +24,12 @@ typedef struct {
 /* process control block type */
 typedef struct pcb_t {
 	/* process queue fields */
-	struct pcb_t *p_next; /* pointer to next entry */
-	struct pcb_t *p_prev; /* pointer to previous  entry */
+	struct pcb_t *p_next,
+	 *p_prev, /* pointer to previous  entry */
 	/* process tree fields */
-		*p_prnt; /* pointer to parent */
-		*p_child; /* pointer to 1st child */
-		*p_sib; /* pointer to sibling */
+		*p_prnt, /* pointer to parent */
+		*p_child, /* pointer to 1st child */
+		*p_sib, /* pointer to sibling */
 		*p_prev_sib; /* pointer to previous sibling */
 	
 	state_t p_s; /* processor state */
