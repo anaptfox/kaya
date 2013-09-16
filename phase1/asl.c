5d04bba829f953b687d773c562e05351712534e1
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-15 21:42:37 -0400

    Restore

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,6 +1,13 @@
 #include "asl.h"
 #include "pcb.c"
-#include "../h/types.h"
+
+/* semaphore descriptor type */
+typedef struct semd_t {
+	struct semd_t *s_next; /* next element on the ASL */
+	int *s_semAdd; /* pointer to the semaphore*/
+	pcb_t *s_procQ; /* tail pointer to a process queue */
+} semd_t;
+
 
 semd_t semd_h;
 semd_t semdFree_h;
--- phase1/pcb.c
+++ phase1/pcb.c
@@ -1,7 +1,24 @@
 #include "../e/pcb.e"
 #include "../h/const.h"
 
-#include "../h/types.h"
+/* process control block type */
+typedef struct pcb_t {
+	/* process queue fields */
+	struct pcb_t *p_next, /* pointer to next  entry */
+	 	*p_prev, /* pointer to previous  entry */
+	/* process tree fields */
+		*p_prnt, /* pointer to parent */
+		*p_child, /* pointer to 1st child */
+		*p_sib, /* pointer to sibling */
+		*p_prev_sib; /* pointer to previous sibling */
+	
+	state_t p_s; /* processor state */
+	
+	int *p_semAdd; /* pointer to sema4 on */
+	/* which process blocked */
+	/* plus other entries to be added later */
+} pcb_t;
+
 
 pcb_t freePcb_tp;
 
