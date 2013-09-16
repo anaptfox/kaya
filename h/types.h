53a95ddd7a0d775712169426d91de2085dd5d082
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-15 21:38:25 -0400

    Fixed underscores

--- h/types.h
+++ h/types.h
@@ -87,28 +87,5 @@ typedef struct state_t {
 #define s_LO	s_reg[30]
 
 
-// /* process control block type */
-// typedef struct pcb_t {
-// 	/* process queue fields */
-// 	struct pcb_t *p_next, /* pointer to next entry */
-// 	/* process tree fields */
-// 	*p_prnt, /* pointer to parent */
-// 	*p_child, /* pointer to 1st child */
-// 	*p_sib; /* pointer to sibling */
-// 	state_t p_s; /* processor state */
-// 	int *p_semAdd;  pointer to sema4 on 
-// 	/* which process blocked */
-// 	/* plus other entries to be added later */
-// } pcb_t;
-
-
-// /* semaphore descriptor type */
-// typedef struct semd_t {
-// 	struct semd_t *s_next; /* next element on the ASL */
-// 	int *s_semAdd; /* pointer to the semaphore*/
-// 	pcb_t *s_procQ; /* tail pointer to a process queue */
-// } semd_t;
-
-
 
 #endif
