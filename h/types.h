6b97bc7b04d3a21832c700b8b96608f0a68977cf
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-15 21:40:03 -0400

    Restore

--- h/types.h
+++ h/types.h
@@ -89,15 +89,29 @@ typedef struct state_t {
 
 /* process control block type */
 typedef struct pcb_t {
+	/* process queue fields */
+	struct pcb_t *p_next, /* pointer to next  entry */
+	 	*p_prev, /* pointer to previous  entry */
+	/* process tree fields */
+		*p_prnt, /* pointer to parent */
+		*p_child, /* pointer to 1st child */
+		*p_sib, /* pointer to sibling */
+		*p_prev_sib; /* pointer to previous sibling */
 	
+	state_t p_s; /* processor state */
+	
+	int *p_semAdd; /* pointer to sema4 on */
+	/* which process blocked */
+	/* plus other entries to be added later */
 } pcb_t;
 
 
 /* semaphore descriptor type */
 typedef struct semd_t {
-	
+	struct semd_t *s_next; /* next element on the ASL */
+	int *s_semAdd; /* pointer to the semaphore*/
+	pcb_t *s_procQ; /* tail pointer to a process queue */
 } semd_t;
-
 
 
 #endif
