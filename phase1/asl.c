ad68251f3f8a0c2cb05d897f2214c90aa0de3ffa
Taron Foxworth <taronfoxworth@gmail.com>
2013-09-16 20:44:01 -0400

    Debuggin

--- phase1/asl.c
+++ phase1/asl.c
@@ -216,14 +216,18 @@ semd_t *removeFree(){
 	if(emptyList(semdFree_h)){
 		return(NULL);
 	}else{
+		debugC(1);
 		semd_t old = (*semdFree_h);
 		if((semdFree_h)->s_next == NULL ){
 			(semdFree_h) = NULL;
 		}else{
 			(semdFree_h) = (semdFree_h)->s_next;
 		}
+		debugC(1);
 		old.s_next = NULL;
+		debugC(1);
 		old.s_procQ = mkEmptyProcQ();
+		debugC(1);
 		return(&old);
 	}
 }
