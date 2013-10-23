2c984004e60b3e1de4f3f7ae4f26a1b1753af550
Robbie <knetscher@xavier.edu>
2013-10-22 21:24:12 -0400

    moving pseudocode to exceptions

--- phase2/interupts.c
+++ phase2/interupts.c
@@ -34,31 +34,4 @@ void inthandler(){
 				// put contents of status in the v0 and put them into the readyQue
 
 }
-//Syscall 1 basic idea, I believe
-void createProcess(){
-	if(free ProcBlk == 0) {
-		$v0 = -1;
-		return $v0;
-	}
-	else{
-		insertProcQ(pcb_t newProcBlk $a1);
-		$v0 = 0;
-		return $v0;
-	}
-}
-//Syscall 2. killemAll should probably either go in terminateJob and is called here,
-//or goes here and is called in terminateJob().
-void terminateProcess(){
-	terminateJob(currentProc);
-}
 
-//When this service (syscall 3) is requested, it is interpreted by the nucleus as a request to
-//perform a V operation on a semaphore. (What is a V operation?)
-void Verhogen(){
-	
-}
-//When this service (syscall 4) is requested, it is interpreted by the nucleus as a request to
-//perform a P operation on a semaphore. (What is a P operation?)
-void Passeren(){
-	
-}
