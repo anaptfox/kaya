3a6ad4f90dde37b579b502033c4d641f97fb3730
Robbie <knetscher@xavier.edu>
2013-10-22 21:32:33 -0400

    moved first four syscall pseudocode to exceptions

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -1,4 +1,3 @@
-
 void syshandler(){
 
 }
@@ -14,4 +13,32 @@ void pgmTrapHandler(){
 
 void TLBHandler(){
 
+}
+//Syscall 1 basic idea, I believe
+void createProcess(){
+	if(free ProcBlk == 0) {
+		$v0 = -1;
+		return $v0;
+	}
+	else{
+		insertProcQ(pcb_t newProcBlk $a1);
+		$v0 = 0;
+		return $v0;
+	}
+}
+//Syscall 2. killemAll should probably either go in terminateJob and is called here,
+//or goes here and is called in terminateJob().
+void terminateProcess(){
+	terminateJob(currentProc);
+}
+
+//When this service (syscall 3) is requested, it is interpreted by the nucleus as a request to
+//perform a V operation on a semaphore. (What is a V operation?)
+void Verhogen(){
+	
+}
+//When this service (syscall 4) is requested, it is interpreted by the nucleus as a request to
+//perform a P operation on a semaphore. (What is a P operation?)
+void Passeren(){
+	
 }
