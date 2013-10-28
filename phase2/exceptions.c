2049718a9a9a6681dac7aa55e700ce41d81e816b
Taron Foxworth <taronfoxworth@gmail.com>
2013-10-28 06:29:59 -0400

    Updated to exceptions and scheduler.c

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -1,5 +1,7 @@
 void syshandler(){
 
+
+
 }
 
 
@@ -11,34 +13,74 @@ void pgmTrapHandler(){
 
 }
 
+
 void TLBHandler(){
 
 }
-//Syscall 1 basic idea, I believe
-void createProcess(){
-	if(free ProcBlk == 0) {
-		$v0 = -1;
-		return $v0;
+
+
+void createProcess( state_t *state){
+	pcb_t *newPcb;
+	if((newPcb = allocPcb()) == NULL){
+		
+		state->s_v0 = -1;
+		
+		return -1;
+	}else{
+		
+		processCnt++; // get a pcb, processcnt++)alloc
+		
+		newPcb->p_s = state->s_a1;// Copy the state pointed by a1 into the p_s of the new pct
+		 	
+		currentProc->currentProc_child = newPcb; // make the newpcb a child of current
+		 	
+		insertProcQ(&readyQue, newPcb);// put the new pcb  on the readyQue
+		
+		state->s_v0 = 0;
+		
+		return 0;
+		
 	}
-	else{
-		insertProcQ(pcb_t newProcBlk $a1);
-		$v0 = 0;
-		return $v0;
-	}
+	
 }
+
 //Syscall 2. killemAll should probably either go in terminateJob and is called here,
 //or goes here and is called in terminateJob().
 void terminateProcess(){
-	terminateJob(currentProc);
+    while(!emptychild(currentProc)){
+        terminateProcess(removeChild(currentProc));     
+    }
+    currentProc = NULL;;
+
 }
 
 //When this service (syscall 3) is requested, it is interpreted by the nucleus as a request to
 //perform a V operation on a semaphore. (What is a V operation?)
-void Verhogen(){
+void Verhogen(int *semaddr){
+
+	pcb_t *p;
+
+	semaddr++;
+
+	if(semaddr <= 0){
+		p = removeBlocked(semaddr);
+		insertProcQ ( & readyQueue ,  p );
+	}
 	
 }
 //When this service (syscall 4) is requested, it is interpreted by the nucleus as a request to
 //perform a P operation on a semaphore. (What is a P operation?)
 void Passeren(){
 	
+	pcb_t *p;
+
+	semaddr--;
+
+	if(semaddr <= -1){
+		insertBlocked (semaddr , currentProcess);
+	}
+}
+
+void getCPUTime(){
+
 }
--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -29,40 +29,8 @@ void scheduler(){
 
 }
 
-void createNewJob(){
-	newPcb = allocPcb();
-	processCnt++; // get a pcb, processcnt++)alloc
-	newPcb->p_s = *a1;// Copy the state pointed by a1 into the p_s of the new pct
-	 	
-	currentProc->currentProc_child = newPcb;// make the newpcb a child of current
-	 	
-	insertProcQ(readyQue->tp newPcb->p_s);// put the new pcb  on the readyQue
-	LDST(&oldsys)
-}
-
-void terminateJob(p){
-                while(!emptychild(p)){
-                       terminateJob(removeChild(p))
-                }
-                if( p == currentProc)
-                        outChild(p);
-                        currentProc = NULL; // can be done out side of kill em all. 
-                if (p->p_semAdd == null){
-                        // on the ready que
-                        outProcQ();
-                }
-                else
-                        // on a sema4
-                        outBlocked();
-                        increment the sema4 by 1 
-                                if it is not on an I/O sema4
-                        decrement softBlkCnt
-                                if it is an I/O sema4
 
 
-                freePcb(p)
-                procCnt -= 1;
-	}
 	
 	// kill current job and all its progeny
 	//recursively travel tree and kill everything (freepcb)
