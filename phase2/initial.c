2e77cd224ce97aa27dc46d7ec5d87da697d836a5
Taron Foxworth <taronfoxworth@gmail.com>
2013-11-01 23:09:46 -0400

    comments fixed

--- phase2/initial.c
+++ phase2/initial.c
@@ -4,9 +4,9 @@
 #include "../e/exceptions.e"
 #include "../e/scheduler.e"
 #include "../e/interrupts.e"
-#include <libumps.e>
+#include "/usr/local/include/umps2/umps/libumps.e"
 
-// Global var
+/* Global var */
 
 pct_t* readyQue;
 
@@ -23,7 +23,7 @@ semd_t pseudo_clock;
 cpu_t startTOD;
 
 
-//Copy before into after
+/* Copy before into after */
 void moveState(state_t *before, state_t* after){
 	after->s_aside = before->s_aside;
 	after->s_cause = before->s_cause;
@@ -42,14 +42,14 @@ void test(void){
 
 int main(void)
 {
-	//inilize the four new areas
+	/* inilize the four new areas */
 
-		//set up devregarea
+		/* set up devregarea*/
 		devregarea_t *devregarea;
 
 		devregarea = (devregarea_t *) 0x10000000;
 
-		//SYSCALLS
+		/* SYSCALLS*/
 		state_t *area = (state_t *)SYS_NEW;
 		STST(area);
 
@@ -57,21 +57,21 @@ int main(void)
 		area->s_sp = devregarea->rambase + devregarea->ramsize;
 		area->status = ALLOFF;
 
-		//ProgramTrap
+		/* ProgramTrap*/
 		area = (state_t *)PGMTRAP_NEW;
 		moveState(area, (state_t *) PGMTRAP_NEW)
 		area->s_pc = p->s_t9 = (memaddr) pgmTrapHandler;
 		area->s_sp = RAMBASEADDR - RAMTOP;
 		area->status = ALLOFF;
 
-		//TLB Management
+		/* TLB Management*/
 		area = (state_t *)TLB_NEW;
 		moveState(area, (state_t *) TLB_NEW)
 		area->s_pc = p->s_t9 = (memaddr) pgmTrapHandler;
 		area->s_sp = RAMBASEADDR - RAMTOP;
 		area->status = ALLOFF;
 
-		//Interrupt
+		/* Interrupt*/
 		area = (state_t *)INT_NEW;
 		moveState(area, (state_t *) INT_NEW)
 		area->s_pc = p->s_t9 = (memaddr) pgmTrapHandler;
@@ -83,7 +83,7 @@ int main(void)
 	initPcbs();
 	initASLs();
 
-	//iniltialize semaphores to 0
+	/* iniltialize semaphores to 0*/
 
 	for(int i=0; i<DEVICE_CNT; i++){
         for(int j=0; j<DEVICE_LINE; j++){
@@ -92,19 +92,19 @@ int main(void)
     }
 
 
-	//Alloc the first pcb
+	/* Alloc the first pcb*/
 	pct_t *p = allocPcb();
 	if(p == NULL){
 		PANIC();
 	}
 	p->s_pc = p->s_t9 = (memaddr) test;
 	p->s_sp = RAMTOP - FRAMESIZE;
-	p->status = 0x0800ff01; //TODO: interrupts enabled, virtual memory off, processor local timer enables, kernel mode on
+	p->status = 0x0800ff01; /* TODO: interrupts enabled, virtual memory off, processor local timer enables, kernel mode on*/
 	mkEmptyProcQ(&readyQue);
 	currentProc = NULL;
 	processCnt = softBlkCnt = 0;
 
-	//populate this pcb
+	/* populate this pcb*/
 	insertProcQ(&readyQue, p);
 	processCnt++;
 	
