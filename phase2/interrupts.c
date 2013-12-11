108c47b3a94cf103a1c52111ab9dfbf660928cf3
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-10 23:24:56 -0500

    debuggin

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -94,8 +94,9 @@ void intHandler(){
 	int terminalRead = 0;
 
 	STCK(endTOD);
+	debugA(10 ,10,10);
 
-	debugA(currentProc->p_time,10,10);
+	debugA(currentProc->p_time ,10,10);
 
 	currentProc->p_time = currentProc->p_time + (endTOD - startTOD);
 
