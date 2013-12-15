500914e4fc84a572d79d21e696cc70728b9e3b32
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-14 20:04:00 -0500

    dummy nodes

--- phase2/initial.c
+++ phase2/initial.c
@@ -63,9 +63,13 @@ int main(void)
 		area = (state_t *)SYS_NEW;
 		STST(area);
 
+		debugMikeyg(10,10,10);
+
 		area->s_pc = area->s_t9 = (memaddr) sysHandler;
 		area->s_sp = devregarea->rambase + devregarea->ramsize;
 		area->s_status = ALLOFF;
+
+		debugMikeyg(10,10,10);
 
 		/* ProgramTrap*/
 		area = (state_t *)PGMTRAP_NEW;
@@ -74,12 +78,16 @@ int main(void)
 		area->s_sp = devregarea->rambase + devregarea->ramsize;
 		area->s_status = ALLOFF;
 
+		debugMikeyg(10,10,10);
+
 		/* TLB Management*/
 		area = (state_t *)TLB_NEW;
 		moveState(area, (state_t *) TLB_NEW);
 		area->s_pc = area->s_t9 = (memaddr) TLBHandler ;
 		area->s_sp = devregarea->rambase + devregarea->ramsize;
 		area->s_status = ALLOFF;
+
+		debugMikeyg(10,10,10);
 
 		/* Interrupt*/
 		area = (state_t *)INT_NEW;
@@ -88,17 +96,29 @@ int main(void)
 		area->s_sp = devregarea->rambase + devregarea->ramsize;
 		area->s_status = ALLOFF;
 
+		debugMikeyg(10,10,10);
+
 	
 
 	initPcbs();
+
+	debugMikeyg(10,10,10);
 	
 	initASL();
 
+	debugMikeyg(10,10,10);
+
 	LDIT(100000);
+
+	debugMikeyg(10,10,10);
 
 	clockSem = 0;
 
+	debugMikeyg(10,10,10);
+
 	setTIMER(5000);
+
+	debugMikeyg(10,10,10);
 
 	/* iniltialize semaphores to 0*/
 	int j;
@@ -113,7 +133,11 @@ int main(void)
   
   }
 
+  debugMikeyg(10,10,10);
+
 	readyQue = mkEmptyProcQ();
+
+	debugMikeyg(10,10,10);
 
 	/* Alloc the first pcb */
 	pcb_t *p = allocPcb();
@@ -124,21 +148,37 @@ int main(void)
 	
 	}
 
+	debugMikeyg(10,10,10);
+
 
 	p->p_s.s_pc = p->p_s.s_t9 = (memaddr) test;
 
+	debugMikeyg(10,10,10);
+
 	p->p_s.s_sp = (devregarea->ramsize + devregarea->rambase) - PAGESIZE;
+
+	debugMikeyg(10,10,10);
 	
 	p->p_s.s_status = p->p_s.s_status | 0x0800ff04;
+
+	debugMikeyg(10,10,10);
 	
 	currentProc = NULL;
+
+	debugMikeyg(10,10,10);
 	
 	processCnt = softBlkCnt = 0;
+
+	debugMikeyg(10,10,10);
 
 	/* populate this pcb*/
 	insertProcQ(&readyQue, p);
 
+	debugMikeyg(10,10,10);
+
 	processCnt = processCnt + 1;
+
+	debugMikeyg(10,10,10);
 
 	
 	scheduler();
