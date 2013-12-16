31d46eb123b34eaca886012a82beb71ac7e5da51
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-16 13:26:24 -0500

    dummy nodes

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -256,18 +256,17 @@ void terminateProcess(pcb_t *p){
 
 	 		debugV(p,10,10);
 
-	 		debugV(p,10,10);
 
 	 		if ( (p->p_semAdd > &(deviceSemas[0][0] ) ) && (p->p_semAdd < &( deviceSemas[DEVICE_LINE][DEVICE_CNT] ) ) ){
 
-	 			debugV(p,10,10);
+	 			debugV(p,10,434234);
 	 		
-	 			softBlkCnt -= 1;
+	 			softBlkCnt = softBlkCnt - 1;
 
 	 			debugV(p,10,570);
 	 		
 	 		}else{
-	 			debugV(p,10,10);
+	 			debugV(p,10,2423423);
 	 		
 	 			*(p->p_semAdd) = *(p->p_semAdd) + 1;
 
