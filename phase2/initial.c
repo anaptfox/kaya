27bccf594821ee92cee07b494def55c8d09ef554
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-15 20:01:08 -0500

    dummy nodes

--- phase2/initial.c
+++ phase2/initial.c
@@ -125,7 +125,7 @@ int main(void)
 
 	p->p_s.s_sp = (devregarea->ramsize + devregarea->rambase) - PAGESIZE;
 
-	p->p_s.s_status = ALLOFF | TE | IM | IEp | KUp;
+	p->p_s.s_status = ALLOFF | 0x0800ff04;
 
 	currentProc = NULL;
 
